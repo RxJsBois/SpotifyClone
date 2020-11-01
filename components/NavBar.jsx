c5a22778b8c2579cb5cfd10fd20c405eb079db75
RoshanJose7 <roshanstark007@gmail.com>
2020-11-01 18:57:16 +0530

    Fix: NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,11 +1,27 @@
+import { Navbar, Nav, Button } from "react-bootstrap";
+
 export default function NavBar() {
   return (
-    <nav className="homePage__navbar">
-      <i class="fab fa-spotify fa-4x" />
-      <h1>Spotify</h1>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <i class="fab fa-spotify fa-3x" />
+      </Navbar.Brand>
+      <h1 style={{ color: "white" }}>Spotify</h1>
+      <Nav style={{ marginLeft: "auto" }}>
+        <Button style={{ margin: "0 10px" }} variant="success">
+          Login
+        </Button>
+        <Button style={{ margin: "0 10px" }} variant="success">
+          SignUp
+        </Button>
+      </Nav>
+    </Navbar>
+    // <nav className="homePage__navbar">
+    //   <i class="fab fa-spotify fa-4x" />
+    //   <h1>Spotify</h1>
 
-      <button className="bg-green-400">Login</button>
-      <button className="bg-green-400">SignUp</button>
-    </nav>
+    //   <button>Login</button>
+    //   <button>SignUp</button>
+    // </nav>
   );
 }
